76b3de2abdc011015c3717b1d5786494a5fd8615
XDaebak <calvinhchoi@gmail.com>
2015-03-06 13:20:16 -0800

    Edited pom.xml to add dependencies. Very messy. Also added jars. JacksonFactory import is not working correctly right now. Added setup code in controller java file.

--- src/main/java/org/oit/MyPortlet.java
+++ src/main/java/org/oit/MyPortlet.java
@@ -13,6 +13,26 @@ import javax.portlet.RenderResponse;
 import javax.portlet.ActionRequest;             
 import javax.portlet.ActionResponse;            
 import javax.portlet.WindowState;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+//import com.google.api.client.json.jackson.JacksonFactory;  // maven can't find this for some reason... 
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.*;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+//import com.google.api.client.http.HttpTransport; // repeated
+import com.google.api.client.json.jackson2.JacksonFactory;
+//import com.google.api.services.calendar.Calendar;  // repeated
+
+import java.io.BufferedReader;
+//import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
 
 public class MyPortlet extends GenericPortlet {
 
@@ -30,12 +50,12 @@ public class MyPortlet extends GenericPortlet {
     public void doView(RenderRequest request, RenderResponse response)
         throws PortletException, IOException 
 	{
-    	PortletPreferences prefs = request.getPreferences();
-    	String content = prefs.getValue("content", "Click edit to edit content."); 
-    	prefs.setValue("content", content);  
+ //   	PortletPreferences prefs = request.getPreferences();
+ //   	String content = prefs.getValue("content", "Click edit to edit content."); 
+ //   	prefs.setValue("content", content);  
     	
-    	request.setAttribute("content", content);
-    	request.setAttribute("mode", request.getPortletMode());
+ //   	request.setAttribute("content", content);
+ //   	request.setAttribute("mode", request.getPortletMode());
 
         if (WindowState.MINIMIZED.equals(request.getWindowState())) 
 		{
@@ -64,11 +84,11 @@ public class MyPortlet extends GenericPortlet {
 	protected void doEdit(RenderRequest request, RenderResponse response)
 		throws PortletException, IOException
 	{
-		PortletPreferences prefs = request.getPreferences();
-		String content = prefs.getValue("content", "Edit content and click submit. Will add cancel button later");
-		request.setAttribute("content", content);
-		request.setAttribute("mode", request.getPortletMode());
-		request.setAttribute("actionURL", response.createActionURL());
+//		PortletPreferences prefs = request.getPreferences();
+//		String content = prefs.getValue("content", "Edit content and click submit. Will add cancel button later");
+//		request.setAttribute("content", content);
+//		request.setAttribute("mode", request.getPortletMode());
+//		request.setAttribute("actionURL", response.createActionURL());
 		
 		editDispatcher.include(request, response);
 	}
@@ -77,11 +97,54 @@ public class MyPortlet extends GenericPortlet {
 	public void processAction(ActionRequest request, ActionResponse response) 
 		throws PortletException, IOException 
 	{
-		String content = request.getParameter("content");
-		PortletPreferences prefs = request.getPreferences();
-		prefs.setValue("content", content);
-		prefs.store();
+//		String content = request.getParameter("content");
+//		PortletPreferences prefs = request.getPreferences();
+//		prefs.setValue("content", content);
+//		prefs.store();
 		response.setPortletMode(PortletMode.VIEW);
+	}
+	
+	public void setUp() throws IOException, GeneralSecurityException 
+	{
+	    HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+	    JacksonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+
+	    // The clientId and clientSecret can be found in Google Developers Console
+	    String clientId = "925906760721-5uvjeol6g2g1ftkr2eeek595hd5u3ut4.apps.googleusercontent.com";
+	    String clientSecret = "notasecret";
+
+	    // Or your redirect URL for web based applications.
+	    String redirectUrl = "urn:ietf:wg:oauth:2.0:oob";
+	    String scope = "https://www.googleapis.com/auth/calendar";
+
+	    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow(
+	        httpTransport, jsonFactory, clientId, clientSecret, Collections.singleton(scope));
+	    // Step 1: Authorize
+	    String authorizationUrl = flow.newAuthorizationUrl().setRedirectUri(redirectUrl).build();
+
+	    // Point or redirect your user to the authorizationUrl.
+	    System.out.println("Go to the following link in your browser:");
+	    System.out.println(authorizationUrl);
+
+	    // Read the authorization code from the standard input stream.
+	    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	    System.out.println("What is the authorization code?");
+	    String code = in.readLine();
+	    // End of Step 1
+
+	    // Step 2: Exchange
+	    GoogleTokenResponse response = flow.newTokenRequest(code).setRedirectUri(redirectUrl)
+	        .execute();
+	    // End of Step 2
+
+	    Credential credential = new GoogleCredential.Builder()
+	        .setTransport(httpTransport)
+	        .setJsonFactory(jsonFactory)
+	        .setClientSecrets(clientId, clientSecret)
+	        .build().setFromTokenResponse(response);
+
+	    Calendar service = new Calendar.Builder(httpTransport, jsonFactory, credential)
+	        .setApplicationName("GoogleCalendar").build();
 	}
 
     public void init(PortletConfig config) throws PortletException 
--- src/main/webapp/normal.jsp
+++ src/main/webapp/normal.jsp
@@ -4,5 +4,13 @@
 <portlet:renderURL portletMode="edit" var="editURL"/>
 <portlet:renderURL portletMode="help" var="helpURL"/>
 
+<iframe src="https://www.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23cc66cc&amp;src=en.usa%23holiday%40group.v.calendar.google.com&amp;color=%2329527A&amp;ctz=America%2FLos_Angeles" 
+		style=" border:solid 1px #777 " 
+		width="800" 
+		height="600" 
+		frameborder="0" 
+		scrolling="no">
+</iframe>
+
 <p>Click to go to edit.jsp: <a href=${editURL}>Edit</a></p>
 <p>Need help? Click <a href=${helpURL}>here.</a></p>
