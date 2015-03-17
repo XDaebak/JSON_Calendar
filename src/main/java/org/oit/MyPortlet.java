a7fa53562d1bf841591b01cc2240e8584e214c59
XDaebak <calvinhchoi@gmail.com>
2015-03-17 15:20:45 -0700

    cleanup

--- src/main/java/org/oit/MyPortlet.java
+++ src/main/java/org/oit/MyPortlet.java
@@ -23,8 +23,6 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
-//import com.google.api.client.json.JsonFactory;
-//import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.json.jackson2.*;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.Calendar.*;
