1b798e7619743c4b1826a1581671d7436327da1b
XDaebak <calvinhchoi@gmail.com>
2015-03-17 15:19:30 -0700

    Modifications in MyPortlet.java are negligible. pom.xml has a lot of dependencies commented out. It seems that only including the google calendar api will also include the google api and everything else.

--- src/main/java/org/oit/MyPortlet.java
+++ src/main/java/org/oit/MyPortlet.java
@@ -24,9 +24,8 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 //import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-//import com.google.api.client.json.jackson2.*;
-
+//import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.jackson2.*;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.Calendar.*;
 
