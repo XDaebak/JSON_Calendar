c500d9684d74e004486134b853e65b91eef1a89c
XDaebak <calvinhchoi@gmail.com>
2015-04-06 14:53:00 -0700

    Added dependency for ical4j. Commented out Google OAuth and ical-to-xml stuff.

--- src/main/java/org/oit/MyPortlet.java
+++ src/main/java/org/oit/MyPortlet.java
@@ -3,6 +3,9 @@ package org.oit;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 
@@ -16,6 +19,14 @@ import javax.portlet.PortletRequestDispatcher;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 import javax.portlet.WindowState;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamWriter;
+
+import net.fortuna.ical4j.data.CalendarParser;
+import net.fortuna.ical4j.data.CalendarParserFactory;
+import net.fortuna.ical4j.data.UnfoldingReader;
+
+import org.oit.XMLHandler;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
@@ -82,8 +93,7 @@ public class MyPortlet extends GenericPortlet
 //		String content = prefs.getValue("content", "Edit content and click submit. Will add cancel button later");
 //		request.setAttribute("content", content);
 //		request.setAttribute("mode", request.getPortletMode());
-//		request.setAttribute("actionURL", response.createActionURL());
-		
+//		request.setAttribute("actionURL", response.createActionURL());	
 		editDispatcher.include(request, response);
 	}
 	
@@ -105,11 +115,11 @@ public class MyPortlet extends GenericPortlet
 	    JacksonFactory jsonFactory = JacksonFactory.getDefaultInstance();
 
 	    // The clientId and clientSecret can be found in Google Developers Console
-	    String clientId = "925906760721-5uvjeol6g2g1ftkr2eeek595hd5u3ut4.apps.googleusercontent.com";
-	    String clientSecret = "notasecret";
+	    String clientId = "925906760721-bc6npb7mhpfbooimc9q3tsar905l2bfv.apps.googleusercontent.com";
+	    String clientSecret = "VQ2FWD3WFkPV7OLtjMoidy45";  // notasecret
 
 	    // Or your redirect URL for web based applications.
-	    String redirectUrl = "urn:ietf:wg:oauth:2.0:oob";
+	    String redirectUrl = "https://github.com/XDaebak"; // urn:ietf:wg:oauth:2.0:oob
 	    String scope = "https://www.googleapis.com/auth/calendar";
 
 	    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow(
@@ -151,17 +161,49 @@ public class MyPortlet extends GenericPortlet
         maximizedDispatcher = config.getPortletContext().getRequestDispatcher(MAXIMIZED_VIEW);
         helpDispatcher = config.getPortletContext().getRequestDispatcher(HELP_VIEW);
 		editDispatcher = config.getPortletContext().getRequestDispatcher(EDIT_MODE);
-		try {
-			setUp();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (GeneralSecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		
+		/*
+		// Testing iCal to XML parser
+		String ICALSTREAM = "BEGIN:VCALENDAR\\r\\nPRODID:-//Sun/Sample//EN\\r\\nVERSION:2.0\\r\\n" + 
+	            "BEGIN:VEVENT\\r\\nUID:1\\r\\nDTSTAMP:20070313T082041Z\\r\\nDTSTART;VALUE=DATE:20081212\\r\\n" +
+	            "SUMMARY:wrapped \\r\\n" +
+	            " summary\\r\\n" +
+	            "END:VEVENT\\r\\nEND:VCALENDAR";
+		
+		Reader reader = new UnfoldingReader(new StringReader(ICALSTREAM));
+        
+        CalendarParser parser = CalendarParserFactory.getInstance().createParser();
+        
+        StringWriter writer = new StringWriter();
+        try 
+        {
+	        XMLStreamWriter xmlWriter = XMLOutputFactory.newInstance().createXMLStreamWriter(writer);
+	        XMLHandler handler = new XMLHandler(xmlWriter);
+	        xmlWriter.writeStartDocument();
+	        parser.parse(reader, handler);
+	        xmlWriter.writeEndDocument();
+	        xmlWriter.close();
+        }
+        catch (Exception e)
+        {
+        	System.out.println("AHAHAHAHA ERROR");
+        }
+        
+        System.out.println("xml representation:" + writer.toString());
+        */
+		
+		// Google Calendar OAuth stuff
+//		try {
+//			setUp();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (GeneralSecurityException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
     }
 
     public void destroy() 
